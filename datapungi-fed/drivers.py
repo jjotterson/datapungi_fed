3fbbc9274072125749e79fd99af9537f22825b05
James Otterson <jjotterson@gmail.com>
2019-09-06 02:09:17 -0400

    new feature: series driver (incomplete)

--- datapungi-fed/drivers.py
+++ datapungi-fed/drivers.py
@@ -117,6 +117,125 @@ def _clipcode(self):
         print("Loaded session does not have a code entry.  Re-run with verbose option set to True. eg: v.drivername(...,verbose=True)")
 
 # (2) Drivers ###################################################################
+class getSeries():
+    def __init__(self,baseRequest={},connectionParameters={},userSettings={}):
+        self._connectionInfo = generalSettings.getGeneralSettings(connectionParameters = connectionParameters, userSettings = userSettings )
+        self._baseRequest    = _getBaseRequest(baseRequest,connectionParameters,userSettings)  
+        self._lastLoad       = {}  #data stored here to assist functions such as clipcode
+    
+    def series(self,
+        series_id              =  '',  
+        api                    =  'observations',             
+        realtime_start         =  '',     
+        realtime_end           =  '',
+        file_type              =  'json',   
+        limit                  =  '', 
+        offset                 =  '',       
+        sort_order             =  '',
+        observation_start      =  '',            
+        observation_end        =  '',
+        units                  =  '',     
+        frequency              =  '',         
+        aggregation_method     =  '',   
+        output_type            =  '',          
+        vintage_dates          =  '',         
+        search_text            =  '',                
+        search_type            =  '',          
+        order_by               =  '',          
+        filter_variable        =  '',             
+        filter_value           =  '',            
+        tag_names              =  '',           
+        exclude_tag_names      =  '',           
+        series_search_text     =  '',    #(fred/series/search/tags or related_tags args)
+        tag_group_id           =  '',    #(fred/series/search/tags or related_tags args)  
+        tag_search_text        =  '',    #(fred/series/search/tags or related_tags args) 
+        params                 = {},
+        verbose                = False,
+        countExceedsLimit      = True
+    ):
+        """
+        ** tags extracts  **  
+        Sample run -
+          {callMethod}()
+        
+        Args:
+            api  (str): choose between "fred/sources" (default), "fred/source", or "fred/source/release"
+                - sources - Get all sources of economic data.
+                - source - Get a source of economic data.
+                - source/releases - Get the releases for a source.
+            source_id (str): default to ''
+            realtime_start (str):  default to '' - which leads to current date
+            realtime_end (str):  default to '' - which leads to current date
+            search_text (str): default to ''
+            limit (str): default to '' - which leads to a limit of 1000 - the max 
+            offset (str): default to ''
+            order_by (str):   default to ''
+            sort_order (str): default to ''
+            file_type (str):  choose between 'json' (default) or 'xml'
+            params (dict):  override all other options with the entries of this dictionary.  (default {})
+            verbose (bool): returns data in a pandas dataframe format (default) or dataframe and all data if True.
+            countExceedsLimit (bool): print warning if dataset size is larger than the download limit
+        Returns:
+            output: either a pandas dataframe or a dictionary (verbose=True) with dataFrame, request, and code              
+        """
+        query = deepcopy(self._baseRequest)
+        
+        #update query url
+        query['url'] = query['url']+'series/'+api
+          
+        #update basequery with passed parameters 
+        allArgs = inspect.getfullargspec(self.series).args
+        localVars = locals()
+        inputParams = { key:localVars[key] for key in allArgs if key not in ['self','api','params','verbose','countExceedsLimit'] } #args that are query params
+        inputParams = dict(filter( lambda entry: entry[1] != '', inputParams.items() )) #filter params.
+        
+        #override if passing arg "params" is non-empty:
+        # - ensure symbols such as + and ; don't get sent to url symbols FED won't read
+        query['params'].update(inputParams)       
+        query['params'].update(params)
+        query['params'] = '&'.join([str(entry[0]) + "=" + str(entry[1]) for entry in query['params'].items()])
+        
+        #get data and clean it
+        retrivedData = requests.get(**query)
+        df_output = self._cleanOutput(api,query,retrivedData)
+        
+        #print warning if there is more data the limit to download
+        _count = retrivedData.json().get('count',1)
+        _limit = retrivedData.json().get('limit',1000)
+        if _count > _limit and countExceedsLimit:
+            print('NOTICE: dataset exceeds download limit! Check - count ({}) and limit ({})'.format(
+                _count,_limit))
+        if verbose == False:
+            self._lastLoad = df_output
+            return(df_output)
+        else:
+            code = _getCode(query,self._connectionInfo.userSettings,self._cleanCode)
+            output = dict(dataFrame = df_output, request = retrivedData, code = code)  
+            self._lastLoad = output
+            return(output)  
+    
+    def _cleanOutput(self,api,query,retrivedData):
+        if   api == "observations":  
+            dataKey = 'observations'
+        elif api == 'series':
+            dataKey = 'series'
+        else:
+            dataKey = 'seriess'
+        self._cleanCode = "df_output =  pd.DataFrame( retrivedData.json()['{}'] )".format(dataKey)
+        df_output =  pd.DataFrame( retrivedData.json()[dataKey] ) #TODO: deal with xml
+        setattr(df_output,'meta', dict(filter( lambda entry: entry[0] != dataKey, retrivedData.json().items() ))) #TODO: silence warning
+        return(df_output)
+        
+    def clipcode(self):
+        _clipcode(self)
+    
+    def _driverMetadata(self):
+        self.metadata =     [{
+            "displayName":"tags",
+            "method"     :"tags",   #Name of driver main function - run with getattr(data,'datasetlist')()
+            "params"     :{ 'file_type': 'json', 'realtime_start': '', 'realtime_end':   '', 'tag_names' : '', 'exclude_tag_names':'', 'tag_group_id': '', 'search_text': '', 'limit':'', 'offset':'', 'order_by':'', 'sort_order':'' },
+        }]
+
 class getSources():
     def __init__(self,baseRequest={},connectionParameters={},userSettings={}):
         self._connectionInfo = generalSettings.getGeneralSettings(connectionParameters = connectionParameters, userSettings = userSettings )
@@ -334,9 +453,13 @@ if __name__ == '__main__':
     #v = d.tags(api='tags/series',tag_names='slovenia;food;oecd')
     
     #sources
-    d = getSources()
+    #d = getSources()
     #v = d.sources()
     #v = d.sources('source','1')
-    v = d.sources('source/releases','1')
-
+    #v = d.sources('source/releases','1')
+    
+    #series
+    d = getSeries()
+    v = d.series('GDP')
     print(v,v.meta)
+    #print(v,v.meta)
