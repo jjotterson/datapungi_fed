6a34f073d406910c94f22533d8c56c076b804aa4
James Otterson <jjotterson@gmail.com>
2019-09-11 23:44:49 -0400

    removed delegator class

--- datapungi_fed/api.py
+++ datapungi_fed/api.py
@@ -1,46 +1,14 @@
 import pandas as pd
 import requests
-from datapungi_fed import generalSettings 
-#import generalSettings 
-from datapungi_fed import drivers
-#import drivers
+#from datapungi_fed import generalSettings 
+import generalSettings 
+#from datapungi_fed import drivers
+import drivers
 
 #TODO: improve delegation (want name of methods - getDatasetlis - to be _get... or be all in a loadedDrivers class etc.  These shouldn't be 
 #      easy for user access)
 # only initialize a driver if it's being called
-
-class delegator(object):
-    def __init__(self):
-        self._lastCalledDriver = ''
-        
-    def __getattr__(self, called_method):
-
-        self._lastCalledMethod = called_method
-
-        def __raise_standard_exception():
-            raise AttributeError("'%s' object has no attribute '%s'" % (self.__class__.__name__, called_method))
-        
-        def wrapper(*args, **kwargs):
-            delegation_config = getattr(self, 'DELEGATED_METHODS', None)
-            if not isinstance(delegation_config, dict):
-                __raise_standard_exception()
-            
-            for delegate_object_str, delegated_methods in delegation_config.items():
-                if called_method in delegated_methods:
-                    break
-            else:
-                __raise_standard_error()
-            
-            delegate_object = getattr(self, delegate_object_str, None)
-            
-            self._lastCalledDriver = delegate_object #NOTE: could use this to track all loaded data etc.  For now, will only use 
-
-            return(getattr(delegate_object, called_method)(*args, **kwargs))
-    
-        return(wrapper)
-
-
-class data(delegator):
+class data():
     '''
        the purpose of this class is to provide an environment where the shared data needed to establish a connection is loaded
        and to be a one stop shop of listing all available drivers.  
@@ -48,31 +16,22 @@ class data(delegator):
          {'key': 'your key', 'description': 'FED data', 'url': ''} 
        :param userSettings: settings saved in the packge pointing to a yaml/json or env containing the connection parameters 
     '''
-    DELEGATED_METHODS = {
-                #'getDatasetlist' :  ['datasetlist'],
-                'getCategories'  :  ['categories'],
-                'getReleases'    :  ['releases'],
-                'getSeries'      :  ['series'],
-                'getSources'     :  ['sources'],
-                'getTags'        :  ['tags'],
-            }
     def __init__(self,connectionParameters = {}, userSettings = {}):
         self.__connectInfo = generalSettings.getGeneralSettings(connectionParameters = connectionParameters, userSettings = userSettings ) 
         self._metadata = self.__connectInfo.packageMetadata
         self._help     = self.__connectInfo.datasourceOverview
         #load drivers:
         loadInfo = {'baseRequest' : self.__connectInfo.baseRequest, 'connectionParameters' : self.__connectInfo.connectionParameters}
-        self.datasetlist   = drivers.datasetlist(**loadInfo)
-        self.getCategories = drivers.getCategories(**loadInfo)
-        self.getReleases   = drivers.getReleases(**loadInfo)
-        self.getSeries     = drivers.getSeries(**loadInfo)
-        self.getSources    = drivers.getSources(**loadInfo)
-        self.getTags       = drivers.getTags(**loadInfo)
+        self.datasetlist  = drivers.datasetlist(**loadInfo)
+        self.categories   = drivers.categories(**loadInfo)
+        self.releases     = drivers.releases(**loadInfo)
+        self.series       = drivers.series(**loadInfo)
+        self.sources      = drivers.sources(**loadInfo)
+        self.tags         = drivers.tags(**loadInfo)
         #self.getGetParameterList           = drivers.getGetParameterList(**loadInfo)
         #TODO: improve loading the drivers 
     
     def __str__(self):
-        print(pd.DataFrame.from_dict(self.DELEGATED_METHODS,orient='index',columns=['Shortcut to Driver']))
         return('\nList of drivers and their shortcuts')
 
     def _clipcode(self):
@@ -89,12 +48,12 @@ class data(delegator):
           returns the __doc__ of the main method inside the driver.
         '''
         #eg: _docDriver('NIPA') 
-        parentName = list(self.DELEGATED_METHODS.keys())[list(self.DELEGATED_METHODS.values()).index([driverName])]
-        outhelp = getattr(getattr(self,parentName ),driverName).__doc__
-        if printHelp:
-            print(outhelp)
-        return(outhelp)
-        
+        #parentName = list(self.DELEGATED_METHODS.keys())[list(self.DELEGATED_METHODS.values()).index([driverName])]
+        #outhelp = getattr(getattr(self,parentName ),driverName).__doc__
+        #if printHelp:
+        #    print(outhelp)
+        #return(outhelp)
+        return('')
 
 
 
@@ -102,4 +61,9 @@ class data(delegator):
 if __name__ == '__main__':
     d = data()
     print(d)
-    print(d.tags())   
+    print(d.datasetlist())   
+    print(d.categories(125))   
+    print(d.releases())   
+    print(d.series('GDP'))
+    print(d.sources('1'))   
+    print(d.tags(tag_names='monetary+aggregates;weekly'))   
